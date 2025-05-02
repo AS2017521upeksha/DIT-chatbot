31e2a56a955e16fb09e5504dfef69581a5ab235f
dwync7 <dwync7@gmail.com>
2025-05-02 23:23:55 +0530

    updated to user satifaction about responses

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 
-import base64
-import os
+
 import google.generativeai as genai
 import streamlit as st
+from datetime import datetime
+from dotenv import load_dotenv
+import os
 
-
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
 
 def generate(input_text, website_context):
     """
@@ -12,15 +15,15 @@ def generate(input_text, website_context):
         input_text (str): The user's query.
         website_context (str): Preloaded or retrieved text from https://mgt.sjp.ac.lk/itc/
     """
-    
 
-    genai.configure(api_key="AIzaSyCTe1kHewjfs3YKf2E_U1WRGALUDa0GoUM")
+
+    genai.configure(api_key=api_key)
 
     model = genai.GenerativeModel("gemini-2.0-flash")
 
     prompt = (f"You are an assistant that answers user questions using the provided website content.\n"
                         f"If the user greets, respond with a friendly greeting.\n"
-                        f"If the answer cannot be found in the website content, say you don't know.\n\n"
+                        f"If the answer cannot be found in the website content, say it's a out of my knowledge scope.\n\n"
                         f"--- Website Content ---\n{website_context}\n"
                         f"--- User Question ---\n{input_text}")
 
@@ -37,7 +40,7 @@ st.set_page_config(page_title="DIT Chatbot", layout="wide")
 # Display the logo and title side by side
 col1, col2 = st.columns([1, 5])
 with col1:
-    st.image("logo.png")  # Adjust width as needed
+    st.image("logo.png")  # Adjust width as needed  
 with col2:
     st.title("Welcome to the DIT GPT")
 
@@ -47,6 +50,7 @@ st.write("Ask me anything related to DIT")
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
+    
 
 # Display chat history
 for message in st.session_state.messages:
@@ -60,18 +64,29 @@ website_context = " "
 with open("itc_website.txt", "r", encoding="utf-8") as f:
     website_context = f.read()
 
+def click_no_button():
+  with open("bot_ns_feedback.txt", "a", encoding="utf-8") as f:
+      f.write(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] NOT SATISFIED: {user_input}\n")
 
 if user_input:
     # Display user message
     st.session_state.messages.append({"role": "user", "content": user_input})
     with st.chat_message("user"):
+
         st.markdown(user_input)
 
     # Get chatbot response using RAG
     bot_response = generate(user_input, website_context)
 
+
     # Display bot response
     st.session_state.messages.append({"role": "assistant", "content": bot_response})
     with st.chat_message("assistant"):
         st.markdown(bot_response)
+        st.write("Are you satisfied with the bot response?")
+        fcol1, fcol2 = st.columns([1, 6])
+        with fcol1:
+           st.button("👍 Yes")
+        with fcol2:
+           st.button("👎 No", on_click=click_no_button)
 
